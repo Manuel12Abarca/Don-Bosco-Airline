dee9af2d33b2847401c2a55cb327751d88cde088
DonBoscoAirlines <131615715+DonBoscoAirlines@users.noreply.github.com>
2023-05-08 21:24:49 -0600

    Add files via upload

---
+++ Don-Bosco-Airline/login/controlador/controlador_login (2).php
@@ -0,0 +1,30 @@
+<?php
+session_start();
+if (!empty($_POST["btningresar"])) {
+    if (!empty($_POST["usuario"]) && !empty($_POST["clave"])) {
+        $usuario = $_POST["usuario"];
+        $password = $_POST["password"];
+        $sql = "SELECT * FROM usuarios WHERE usuario='$usuario' AND clave='$password'";
+        $result = $conexion->query($sql);
+        if(mysqli_num_rows($result) == 1) {
+            // Los datos del usuario son almacenados
+            $row = mysqli_fetch_assoc($result);
+            $_SESSION["id"]=$row["id"];
+            $_SESSION["nombre"]=$row["nombre"];
+            $_SESSION["apellido"]=$row["apellido"];
+            // El usuario existe en la base de datos
+            // Redireccionar al usuario a la página principal
+            header("Location:../../index.html");
+            exit();
+        } else {
+            // El usuario no existe en la base de datos
+            // Mostrar un mensaje de error
+            echo "Los datos no coinciden";
+        }
+        
+    } else {
+        echo "Completa todos los campos";
+    }
+}
+
+?>
