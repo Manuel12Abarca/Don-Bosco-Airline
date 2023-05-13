b1a78d0f9353350588e5f88e981bef79b992950e
Manuel12Abarca <112524213+Manuel12Abarca@users.noreply.github.com>
2023-05-13 17:20:14 -0600

    Traducción

--- Reserva/destino.php
+++ Reserva/destino.php
@@ -66,7 +66,7 @@
 		</div>
 	</nav>
 	
-	<header id="gtco-header" class="gtco-cover gtco-cover-md" role="banner" style="background-image: url(../Reserva/images/fofo.jpg)">
+	<header id="gtco-header" class="gtco-cover gtco-cover-md" role="banner" style="background-image: url(images/img_bg_2.jpg)">
 		<div class="overlay"></div>
 		<div class="gtco-container">
 			<div class="row">
@@ -107,10 +107,10 @@
 														<label for="destination">Destino</label>
 														<select name="destino" id="destination" class="form-control" required="">
 															<option value="Colombia">Colombia</option>
-															<option value="Philippines">Philippines</option>
+															<option value="Philippines">Filipinas</option>
 															<option value="USA">USA</option>
 															<option value="Australia">Australia</option>
-															<option value="Singapore">Singapore</option>
+															<option value="Singapore">Singapur</option>
 														</select>
 													</div>
 												</div>
@@ -145,12 +145,13 @@
 	
 	<div class="gtco-section">
 		<div class="gtco-container">
-			<div class="row">
-				<div class="col-md-8 col-md-offset-2 text-center gtco-heading">
-					<h2>Most Popular Destination</h2>
-					<p>Dignissimos asperiores vitae velit veniam totam fuga molestias accusamus alias autem provident. Odit ab aliquam dolor eius.</p>
-				</div>
-			</div>
+		 <div class="row">
+             <div class="col-md-8 col-md-offset-2 text-center gtco-heading">
+                  <h2>Destinos más Populares</h2>
+                  <p>Descubre los destinos más populares para viajar y disfrutar de tus vacaciones.</p>
+             </div>
+        </div>
+
 			<div class="row">
 
 				<div class="col-lg-4 col-md-4 col-sm-6">
@@ -160,10 +161,12 @@
 							<img src="images/img_1.jpg" alt="Image" class="img-responsive">
 						</figure>
 						<div class="fh5co-text">
-							<h2>New York, USA</h2>
-							<p>Far far away, behind the word mountains, far from the countries Vokalia..</p>
-							<p><span class="btn btn-primary">Schedule a Trip</span></p>
-						</div>
+                           <h2>Nueva York, EE. UU.</h2>
+                           <p>Nueva York, la ciudad que nunca duerme, es un centro cultural y financiero de renombre mundial. Conocida por sus icónicos rascacielos, como el Empire State Building y el One World Trade Center, así como por sus famosos lugares de interés, como Times Square y Central Park, Nueva York ofrece una experiencia inigualable. Disfruta de la vibrante vida nocturna, explora los diversos barrios, como Manhattan y Brooklyn, y sumérgete en la cultura a través de sus museos, teatros y restaurantes de clase mundial. Planifica un viaje a Nueva York y descubre todo lo que esta fascinante ciudad tiene para ofrecer.</p>
+                           <p><span class="btn btn-primary">Planifica un Viaje</span></p>
+                        </div>
+
+
 					</a>
 				</div>
 				<div class="col-lg-4 col-md-4 col-sm-6">
@@ -173,10 +176,12 @@
 							<img src="images/img_2.jpg" alt="Image" class="img-responsive">
 						</figure>
 						<div class="fh5co-text">
-							<h2>Seoul, South Korea</h2>
-							<p>Far far away, behind the word mountains, far from the countries Vokalia..</p>
-							<p><span class="btn btn-primary">Schedule a Trip</span></p>
-						</div>
+                          <h2>Seúl, Corea del Sur</h2>
+                          <p>Seúl, la capital de Corea del Sur, es una ciudad vibrante y moderna que combina una rica historia con avances tecnológicos. Con una mezcla única de tradición y modernidad, Seúl ofrece una amplia variedad de atracciones para los visitantes. Pasea por los palacios reales, explora los bulliciosos mercados callejeros, sumérgete en la animada escena de la moda y disfruta de la deliciosa comida coreana. Además, la ciudad cuenta con una excelente infraestructura de transporte y una amplia gama de opciones de entretenimiento. Descubre la fascinante cultura y energía de Seúl en tu próximo viaje.</p>
+                          <p><span class="btn btn-primary">Planifica un Viaje</span></p>
+                        </div>
+
+
 					</a>
 				</div>
 				<div class="col-lg-4 col-md-4 col-sm-6">
@@ -186,10 +191,11 @@
 							<img src="images/img_3.jpg" alt="Image" class="img-responsive">
 						</figure>
 						<div class="fh5co-text">
-							<h2>Paris, France</h2>
-							<p>Far far away, behind the word mountains, far from the countries Vokalia..</p>
-							<p><span class="btn btn-primary">Schedule a Trip</span></p>
-						</div>
+                          <h2>París, Francia</h2>
+                          <p>París, la Ciudad de la Luz, es famosa por su belleza, su romance y su rica historia. Con sus icónicos monumentos, como la Torre Eiffel, el Louvre y la Catedral de Notre Dame, París cautiva a sus visitantes con su arquitectura impresionante y su encanto único. Disfruta de paseos románticos a orillas del río Sena, explora los encantadores barrios como Montmartre y disfruta de la exquisita gastronomía francesa en los cafés y restaurantes tradicionales. Sumérgete en el arte y la cultura en los numerosos museos y galerías de la ciudad. Planifica un viaje a París y descubre la magia de esta ciudad inolvidable.</p>
+                          <p><span class="btn btn-primary">Planifica un Viaje</span></p>
+                        </div>
+
 					</a>
 				</div>
 
@@ -201,10 +207,11 @@
 							<img src="images/img_4.jpg" alt="Image" class="img-responsive">
 						</figure>
 						<div class="fh5co-text">
-							<h2>Sydney, Australia</h2>
-							<p>Far far away, behind the word mountains, far from the countries Vokalia..</p>
-							<p><span class="btn btn-primary">Schedule a Trip</span></p>
-						</div>
+                           <h2>Sídney, Australia</h2>
+                           <p>Sídney, la icónica ciudad de Australia, es conocida por sus impresionantes playas, su famoso puerto y su estilo de vida relajado. Con el emblemático Sydney Opera House y el Sydney Harbour Bridge, la ciudad ofrece vistas panorámicas inolvidables. Disfruta de las hermosas playas, como Bondi Beach, y practica actividades acuáticas como el surf o el buceo. Explora los diversos barrios de la ciudad, desde el animado distrito central de negocios hasta las pintorescas áreas costeras. Además, Sídney ofrece una escena cultural vibrante, con museos, galerías de arte y una gran variedad de opciones gastronómicas. Planifica un viaje a Sídney y descubre la belleza y el encanto de esta ciudad australiana.</p>
+                           <p><span class="btn btn-primary">Planifica un Viaje</span></p>
+                        </div>
+
 					</a>
 				</div>
 
@@ -215,10 +222,11 @@
 							<img src="images/img_5.jpg" alt="Image" class="img-responsive">
 						</figure>
 						<div class="fh5co-text">
-							<h2>Greece, Europe</h2>
-							<p>Far far away, behind the word mountains, far from the countries Vokalia..</p>
-							<p><span class="btn btn-primary">Schedule a Trip</span></p>
-						</div>
+                          <h2>Grecia, Europa</h2>
+                          <p>Grecia, cuna de la civilización occidental, es un país fascinante ubicado en Europa. Con su rica historia, impresionantes ruinas antiguas y hermosas islas, Grecia ofrece una experiencia cultural única. Explora los sitios arqueológicos emblemáticos, como el Partenón en Atenas y los antiguos teatros en Epidauro y Delfos. Disfruta de las playas de aguas cristalinas en las islas griegas, como Mykonos y Santorini, y sumérgete en la encantadora cultura mediterránea. Además, la gastronomía griega es famosa por sus sabores auténticos y deliciosos. Planifica un viaje a Grecia y descubre la riqueza histórica y la belleza natural de este maravilloso país europeo.</p>
+                          <p><span class="btn btn-primary">Planifica un Viaje</span></p>
+                        </div>
+
 					</a>
 				</div>
 
@@ -229,10 +237,11 @@
 							<img src="images/img_6.jpg" alt="Image" class="img-responsive">
 						</figure>
 						<div class="fh5co-text">
-							<h2>Spain, Europe</h2>
-							<p>Far far away, behind the word mountains, far from the countries Vokalia..</p>
-							<p><span class="btn btn-primary">Schedule a Trip</span></p>
-						</div>
+                            <h2>España, Europa</h2>
+                            <p>España, situada en Europa, es un país lleno de encanto y diversidad. Con su rica historia, hermosas playas y vibrante cultura, España cautiva a los visitantes de todo el mundo. Descubre las majestuosas ciudades como Madrid, Barcelona y Sevilla, que combinan arquitectura impresionante con una animada vida nocturna. Explora las hermosas costas y disfruta del sol en las playas mediterráneas y atlánticas. Sumérgete en la cultura española a través de su deliciosa gastronomía, festivales coloridos y pasión por el fútbol. Planifica un viaje a España y déjate seducir por la belleza y el espíritu acogedor de este país europeo.</p>
+                            <p><span class="btn btn-primary">Planifica un Viaje</span></p>
+                        </div>
+
 					</a>
 				</div>
 
@@ -244,8 +253,7 @@
 		<div class="gtco-container">
 			<div class="row">
 				<div class="col-md-8 col-md-offset-2 text-center gtco-heading animate-box">
-					<h2>How It Works</h2>
-					<p>Dignissimos asperiores vitae velit veniam totam fuga molestias accusamus alias autem provident. Odit ab aliquam dolor eius.</p>
+				   <h2>¿Como funciona?</h2>
 				</div>
 			</div>
 			<div class="row">
@@ -254,8 +262,8 @@
 						<span class="icon">
 							<i>1</i>
 						</span>
-						<h3>Lorem ipsum dolor sit amet</h3>
-						<p>Dignissimos asperiores vitae velit veniam totam fuga molestias accusamus alias autem provident. Odit ab aliquam dolor eius.</p>
+						<h3>Búsqueda y selección de vuelos</h3>
+						<p>El sistema de reservas de vuelo permite a los usuarios buscar y seleccionar vuelos de acuerdo con sus preferencias. Los usuarios pueden ingresar los detalles de origen, destino, fechas de viaje y número de pasajeros para obtener una lista de opciones disponibles.</p>
 					</div>
 				</div>
 				<div class="col-md-4 col-sm-6">
@@ -263,8 +271,8 @@
 						<span class="icon">
 							<i>2</i>
 						</span>
-						<h3>Consectetur adipisicing elit</h3>
-						<p>Dignissimos asperiores vitae velit veniam totam fuga molestias accusamus alias autem provident. Odit ab aliquam dolor eius.</p>
+						<h3>Reserva y confirmación</h3>
+						<p> Una vez que los usuarios eligen un vuelo deseado, el sistema les permite reservarlo. Los usuarios proporcionan la información necesaria, como nombres de los pasajeros, información de contacto y preferencias de asiento.</p>
 					</div>
 				</div>
 				<div class="col-md-4 col-sm-6">
@@ -272,8 +280,8 @@
 						<span class="icon">
 							<i>3</i>
 						</span>
-						<h3>Dignissimos asperiores vitae</h3>
-						<p>Dignissimos asperiores vitae velit veniam totam fuga molestias accusamus alias autem provident. Odit ab aliquam dolor eius.</p>
+						<h3>Gestión y pagos</h3>
+						<p> El sistema de reservas de vuelo también ofrece funciones de gestión para los usuarios. Permite a los usuarios realizar cambios en las reservas existentes, como modificar fechas, agregar o eliminar pasajeros o seleccionar asientos preferidos.</p>
 					</div>
 				</div>
 				
@@ -288,7 +296,7 @@
 		<div class="gtco-container text-center">
 			<div class="display-t">
 				<div class="display-tc">
-					<h1>We have high quality services that you will surely love!</h1>
+					<h1>¡Contamos con servicios de alta calidad que seguro te encantarán!</h1>
 				</div>	
 			</div>
 		</div>
@@ -298,10 +306,11 @@
 		<div class="gtco-container">
 
 			<div class="row">
-				<div class="col-md-8 col-md-offset-2 text-center gtco-heading animate-box">
-					<h2>Our Success</h2>
-					<p>Dignissimos asperiores vitae velit veniam totam fuga molestias accusamus alias autem provident. Odit ab aliquam dolor eius.</p>
-				</div>
+			<div class="col-md-8 col-md-offset-2 text-center gtco-heading animate-box">
+               <h2>Nuestro Éxito</h2>
+                <p>Nuestro éxito se basa en la dedicación y el esfuerzo constante para ofrecer los mejores servicios. Trabajamos arduamente para superar las expectativas de nuestros clientes y brindarles experiencias inolvidables. Valoramos la confianza que nuestros clientes depositan en nosotros y nos esforzamos por mantenerla en cada paso del camino. Estamos comprometidos con la excelencia y siempre buscamos mejorar y crecer. Gracias a nuestro equipo altamente capacitado y a nuestra pasión por lo que hacemos, hemos logrado alcanzar resultados excepcionales. Estamos orgullosos de nuestro éxito y seguiremos trabajando arduamente para mantenerlo.</p>
+            </div>
+
 			</div>
 
 			<div class="row">
@@ -309,26 +318,26 @@
 				<div class="col-md-3 col-sm-6 animate-box" data-animate-effect="fadeInUp">
 					<div class="feature-center">
 						<span class="counter js-counter" data-from="0" data-to="196" data-speed="5000" data-refresh-interval="50">1</span>
-						<span class="counter-label">Destination</span>
+						<span class="counter-label">Destinaciones</span>
 
 					</div>
 				</div>
 				<div class="col-md-3 col-sm-6 animate-box" data-animate-effect="fadeInUp">
 					<div class="feature-center">
 						<span class="counter js-counter" data-from="0" data-to="97" data-speed="5000" data-refresh-interval="50">1</span>
-						<span class="counter-label">Hotels</span>
+						<span class="counter-label">Hoteles</span>
 					</div>
 				</div>
 				<div class="col-md-3 col-sm-6 animate-box" data-animate-effect="fadeInUp">
 					<div class="feature-center">
 						<span class="counter js-counter" data-from="0" data-to="12402" data-speed="5000" data-refresh-interval="50">1</span>
-						<span class="counter-label">Travelers</span>
+						<span class="counter-label">Viajeros</span>
 					</div>
 				</div>
 				<div class="col-md-3 col-sm-6 animate-box" data-animate-effect="fadeInUp">
 					<div class="feature-center">
 						<span class="counter js-counter" data-from="0" data-to="12202" data-speed="5000" data-refresh-interval="50">1</span>
-						<span class="counter-label">Happy Customer</span>
+						<span class="counter-label">Clientes felices</span>
 
 					</div>
 				</div>
@@ -339,94 +348,21 @@
 
 	
 
-	<div id="gtco-subscribe">
-		<div class="gtco-container">
-			<div class="row animate-box">
-				<div class="col-md-8 col-md-offset-2 text-center gtco-heading">
-					<h2>Subscribe</h2>
-					<p>Be the first to know about the new templates.</p>
-				</div>
-			</div>
-			<div class="row animate-box">
-				<div class="col-md-8 col-md-offset-2">
-					<form class="form-inline">
-						<div class="col-md-6 col-sm-6">
-							<div class="form-group">
-								<label for="email" class="sr-only">Email</label>
-								<input type="email" class="form-control" id="email" placeholder="Your Email">
-							</div>
-						</div>
-						<div class="col-md-6 col-sm-6">
-							<button type="submit" class="btn btn-default btn-block">Subscribe</button>
-						</div>
-					</form>
-				</div>
-			</div>
-		</div>
-	</div>
-
 	<footer id="gtco-footer" role="contentinfo">
 		<div class="gtco-container">
-			<div class="row row-p	b-md">
-
-				<div class="col-md-4">
-					<div class="gtco-widget">
-						<h3>About Us</h3>
-						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore eos molestias quod sint ipsum possimus temporibus officia iste perspiciatis consectetur in fugiat repudiandae cum. Totam cupiditate nostrum ut neque ab?</p>
-					</div>
-				</div>
-
-				<div class="col-md-2 col-md-push-1">
-					<div class="gtco-widget">
-						<h3>Destination</h3>
-						<ul class="gtco-footer-links">
-							<li><a href="#">Europe</a></li>
-							<li><a href="#">Australia</a></li>
-							<li><a href="#">Asia</a></li>
-							<li><a href="#">Canada</a></li>
-							<li><a href="#">Dubai</a></li>
-						</ul>
-					</div>
-				</div>
-
-				<div class="col-md-2 col-md-push-1">
-					<div class="gtco-widget">
-						<h3>Hotels</h3>
-						<ul class="gtco-footer-links">
-							<li><a href="#">Luxe Hotel</a></li>
-							<li><a href="#">Italy 5 Star hotel</a></li>
-							<li><a href="#">Dubai Hotel</a></li>
-							<li><a href="#">Deluxe Hotel</a></li>
-							<li><a href="#">BoraBora Hotel</a></li>
-						</ul>
-					</div>
-				</div>
-
-				<div class="col-md-3 col-md-push-1">
-					<div class="gtco-widget">
-						<h3>Get In Touch</h3>
-						<ul class="gtco-quick-contact">
-							<li><a href="#"><i class="icon-phone"></i> +1 234 567 890</a></li>
-							<li><a href="#"><i class="icon-mail2"></i> info@freehtml5.co</a></li>
-							<li><a href="#"><i class="icon-chat"></i> Live Chat</a></li>
-						</ul>
-					</div>
-				</div>
-
-			</div>
 
 			<div class="row copyright">
 				<div class="col-md-12">
 					<p class="pull-left">
 						<small class="block">&copy; 2023</small> 
-						<small class="block">Designed by <a href="https://freehtml5.co/" target="_blank">FreeHTML5.co</a> Demo Images: <a href="http://unsplash.com/" target="_blank">Unsplash</a></small>
+						<small class="block">SISTEMA RESERVAS AVIÓN <a>Air Don Bosco</a></small>
 					</p>
 					<p class="pull-right">
 						<ul class="gtco-social-icons pull-right">
-							<li><a href="#"><i class="icon-twitter"></i></a></li>
-							<li><a href="#"><i class="icon-facebook"></i></a></li>
-							<li><a href="#"><i class="icon-linkedin"></i></a></li>
-							<li><a href="#"><i class="icon-dribbble"></i></a></li>
+							<li><a href="https://www.youtube.com/channel/UC87BrnQy7Y6iipGvQ6sAJfQ"><i class="icon-twitter"></i></a></li>
+							<li><a href="https://www.facebook.com/jhonatan.cardona.9237"><i class="icon-facebook"></i></a></li>
+							<li><a href="https://www.youtube.com/channel/UC87BrnQy7Y6iipGvQ6sAJfQ"><i class="icon-linkedin"></i></a></li>
+							<li><a href="https://www.youtube.com/channel/UC87BrnQy7Y6iipGvQ6sAJfQ"><i class="icon-dribbble"></i></a></li>
 						</ul>
 					</p>
 				</div>
